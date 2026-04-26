22e27e7cf7830e4e7e86560ac34d5c4d5a02310f
laithethanh <laithanh1006@gmail.com>
2026-04-26 15:25:48 +0700

    add cor config for PATCH

--- backend/src/main/java/com/shopcart/config/SecurityConfig.java
+++ backend/src/main/java/com/shopcart/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
                                 "http://localhost:5173",
                                 "http://localhost:3000"));
                 configuration.setAllowedMethods(Arrays.asList(
-                                "GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                                "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(List.of("*"));
                 configuration.setAllowCredentials(true);
                 configuration.setMaxAge(3600L);
