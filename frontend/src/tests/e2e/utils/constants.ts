2249a6f689ffa7991c8d46c52eb77254646f86a5
baophan <baop00905@gmail.com>
2026-05-10 02:19:48 +0700

    test ci github

--- frontend/src/tests/e2e/utils/constants.ts
+++ frontend/src/tests/e2e/utils/constants.ts
@@ -1,6 +1,6 @@
 /**
  * Constants: Chứa tất cả các hằng số sử dụng trong E2E tests
- * Bao gồm cấu hình API, phí vận chuyển, và dữ liệu test mẫu
+ * Bao gồm cấu hình API, phí vận chuyển, và dữ liệu test mẫu.
  */
 
 export const SHIPPING_FEE = 29900;
